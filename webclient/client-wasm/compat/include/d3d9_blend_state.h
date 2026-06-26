4bc4e97d6a610c5d1c5a49f7cc3ef33a4b65259d
alanpetry <alan_petry@hotmail.com>
2026-06-26 02:06:45 -0300

    Expand D3D9 blend helper for separate alpha state

--- webclient/client-wasm/compat/include/d3d9_blend_state.h
+++ webclient/client-wasm/compat/include/d3d9_blend_state.h
@@ -2,9 +2,6 @@
 
 #include "d3d9.h"
 
-// Shared D3D9 blend-state mapping for the WASM/WebGL compatibility path.
-// Keep this header free of GL includes so it can be used by state tests and by
-// win32_emscripten_stubs.cpp without forcing GLES types into d3d9.h.
 namespace wyd::d3d9_compat {
 
 enum class WebGLBlendFactor : DWORD {
@@ -31,6 +28,16 @@ enum class WebGLBlendEquation : DWORD {
   Max = 0x8008,
 };
 
+struct WebGLBlendState {
+  WebGLBlendFactor src_rgb = WebGLBlendFactor::SrcAlpha;
+  WebGLBlendFactor dst_rgb = WebGLBlendFactor::OneMinusSrcAlpha;
+  WebGLBlendFactor src_alpha = WebGLBlendFactor::SrcAlpha;
+  WebGLBlendFactor dst_alpha = WebGLBlendFactor::OneMinusSrcAlpha;
+  WebGLBlendEquation rgb_op = WebGLBlendEquation::Add;
+  WebGLBlendEquation alpha_op = WebGLBlendEquation::Add;
+  DWORD blend_factor = 0xFFFFFFFFu;
+};
+
 inline WebGLBlendFactor BlendFactorToWebGL(DWORD blend) {
   switch (blend) {
     case D3DBLEND_ZERO:
@@ -42,8 +49,10 @@ inline WebGLBlendFactor BlendFactorToWebGL(DWORD blend) {
     case D3DBLEND_INVSRCCOLOR:
       return WebGLBlendFactor::OneMinusSrcColor;
     case D3DBLEND_SRCALPHA:
+    case D3DBLEND_BOTHSRCALPHA:
       return WebGLBlendFactor::SrcAlpha;
     case D3DBLEND_INVSRCALPHA:
+    case D3DBLEND_BOTHINVSRCALPHA:
       return WebGLBlendFactor::OneMinusSrcAlpha;
     case D3DBLEND_DESTALPHA:
       return WebGLBlendFactor::DstAlpha;
@@ -59,8 +68,6 @@ inline WebGLBlendFactor BlendFactorToWebGL(DWORD blend) {
       return WebGLBlendFactor::ConstantColor;
     case D3DBLEND_INVBLENDFACTOR:
       return WebGLBlendFactor::OneMinusConstantColor;
-    case D3DBLEND_BOTHSRCALPHA:
-    case D3DBLEND_BOTHINVSRCALPHA:
     case D3DBLEND_SRCCOLOR2:
     case D3DBLEND_INVSRCCOLOR2:
     default:
@@ -82,6 +89,45 @@ inline WebGLBlendEquation BlendOpToWebGL(DWORD op) {
     default:
       return WebGLBlendEquation::Add;
   }
+}
+
+inline bool BlendFactorUsesConstantColor(WebGLBlendFactor factor) {
+  return factor == WebGLBlendFactor::ConstantColor ||
+         factor == WebGLBlendFactor::OneMinusConstantColor;
+}
+
+inline bool BlendStateUsesConstantColor(const WebGLBlendState& state) {
+  return BlendFactorUsesConstantColor(state.src_rgb) ||
+         BlendFactorUsesConstantColor(state.dst_rgb) ||
+         BlendFactorUsesConstantColor(state.src_alpha) ||
+         BlendFactorUsesConstantColor(state.dst_alpha);
+}
+
+inline WebGLBlendState BuildWebGLBlendState(DWORD src_blend,
+                                            DWORD dst_blend,
+                                            DWORD blend_op,
+                                            DWORD blend_factor,
+                                            bool separate_alpha_blend_enable,
+                                            DWORD src_blend_alpha,
+                                            DWORD dst_blend_alpha,
+                                            DWORD blend_op_alpha) {
+  WebGLBlendState state;
+  state.src_rgb = BlendFactorToWebGL(src_blend);
+  state.dst_rgb = BlendFactorToWebGL(dst_blend);
+  state.rgb_op = BlendOpToWebGL(blend_op);
+  state.blend_factor = blend_factor;
+
+  if (separate_alpha_blend_enable) {
+    state.src_alpha = BlendFactorToWebGL(src_blend_alpha);
+    state.dst_alpha = BlendFactorToWebGL(dst_blend_alpha);
+    state.alpha_op = BlendOpToWebGL(blend_op_alpha);
+  } else {
+    state.src_alpha = state.src_rgb;
+    state.dst_alpha = state.dst_rgb;
+    state.alpha_op = state.rgb_op;
+  }
+
+  return state;
 }
 
 }  // namespace wyd::d3d9_compat
