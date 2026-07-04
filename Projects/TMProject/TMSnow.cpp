2d25f07b4d2f8d85372a5b3df2729a2bc77ea4c1
alanpetry <alan_petry@hotmail.com>
2026-07-04 13:08:04 -0300

    Restore fog state after snow render pass

--- Projects/TMProject/TMSnow.cpp
+++ Projects/TMProject/TMSnow.cpp
@@ -147,6 +147,7 @@ int TMSnow::Render()
     }
 
     g_pDevice->SetRenderState(D3DRS_LIGHTING, 1u);
+    g_pDevice->SetRenderState(D3DRS_FOGENABLE, g_pDevice->m_bFog);
     g_pDevice->SetRenderState(D3DRS_SRCBLEND, 2u);
     g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 7u);
     g_pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, 1u);
