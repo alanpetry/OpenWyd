5d7f65cfb486c5499481d0d7f0b45bc11e1d4ecb
alanpetry <alan_petry@hotmail.com>
2026-07-11 09:08:14 -0300

    Restore Billboard2 render state after draw

--- Projects/TMProject/TMEffectBillBoard2.cpp
+++ Projects/TMProject/TMEffectBillBoard2.cpp
@@ -83,7 +83,7 @@ int TMEffectBillBoard2::Render()
 		
 		g_pDevice->SetTexture(0, g_pTextureManager->GetEffectTexture(m_nTextureIndex, 5000));
 
-		g_pDevice->m_pd3dDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2u, m_vertex, 24u);
+		HRESULT hr = g_pDevice->m_pd3dDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2u, m_vertex, 24u);
 
 		g_pDevice->SetRenderState(D3DRS_CULLMODE, 3u);
 		g_pDevice->SetRenderState(D3DRS_LIGHTING, 1u);
@@ -91,9 +91,15 @@ int TMEffectBillBoard2::Render()
 		g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 7u);
 		g_pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, 1u);
 		g_pDevice->SetRenderState(D3DRS_ZWRITEENABLE, 1u);
+		g_pDevice->SetRenderState(D3DRS_FOGENABLE, g_pDevice->m_bFog);
 
 		if (m_efAlphaType == EEFFECT_ALPHATYPE::EF_BRIGHT)
 			g_pDevice->SetRenderState(D3DRS_DESTBLEND, 6u);
+		else
+			g_pDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, 2u);
+
+		if (hr < 0)
+			return 0;
 	}
 	return 1;
 }
