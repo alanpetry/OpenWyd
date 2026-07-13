00693458e48d163d71c5fca550c88fc68f241cb3
alanpetry <alan_petry@hotmail.com>
2026-07-13 19:05:12 -0300

    Restore billboard render state after FVF draw

--- Projects/TMProject/TMEffectBillBoard.cpp
+++ Projects/TMProject/TMEffectBillBoard.cpp
@@ -154,10 +154,10 @@ int TMEffectBillBoard::Render()
 
 		g_pDevice->SetTexture(0, g_pTextureManager->GetEffectTexture(m_nCycleIndex + m_nTextureIndex, 5000u));
 
-		if (g_pDevice->m_pd3dDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2u, m_vertex, 24u) < 0) 
-			return 0;
+		HRESULT hr = g_pDevice->m_pd3dDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2u, m_vertex, 24u);
 
 		g_pDevice->SetRenderState(D3DRS_CULLMODE, 3u);
+		g_pDevice->SetRenderState(D3DRS_FOGENABLE, g_pDevice->m_bFog);
 		g_pDevice->SetRenderState(D3DRS_LIGHTING, 1u);
 		g_pDevice->SetRenderState(D3DRS_SRCBLEND, 2u);
 		g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 7u);
@@ -168,6 +168,9 @@ int TMEffectBillBoard::Render()
 			g_pDevice->SetRenderState(D3DRS_DESTBLEND, 6u);
 		else
 			g_pDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, 2u);
+
+		if (hr < 0)
+			return 0;
 	}
 	return 1;
 }
