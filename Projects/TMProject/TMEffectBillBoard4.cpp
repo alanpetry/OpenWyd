98e95ab1f70a9881aefc2689b779e1f01274c7ad
alanpetry <alan_petry@hotmail.com>
2026-07-18 07:03:12 -0300

    Restore Billboard4 render state after draw

--- Projects/TMProject/TMEffectBillBoard4.cpp
+++ Projects/TMProject/TMEffectBillBoard4.cpp
@@ -93,11 +93,9 @@ int TMEffectBillBoard4::Render()
         g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 7u);
         g_pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, 1u);
         g_pDevice->SetRenderState(D3DRS_ZWRITEENABLE, 1u);
-
-        if (m_efAlphaType == EEFFECT_ALPHATYPE::EF_BRIGHT)
-            g_pDevice->SetRenderState(D3DRS_DESTBLEND, 6u);
-        else
-            g_pDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, 2u);
+        g_pDevice->SetRenderState(D3DRS_DESTBLEND, 6u);
+        g_pDevice->SetRenderState(D3DRS_FOGENABLE, g_pDevice->m_bFog);
+        g_pDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, 2u);
     }
     return 1;
 }
