f877160cb895b17c910f16e70cdc95c509909ad4
alanpetry <alan_petry@hotmail.com>
2026-07-21 22:10:06 -0300

    Use Billboard3 horizontal scale for crossed quad width

--- Projects/TMProject/TMEffectBillBoard3.cpp
+++ Projects/TMProject/TMEffectBillBoard3.cpp
@@ -26,10 +26,10 @@ TMEffectBillBoard3::TMEffectBillBoard3(TMVector3 vecStart, TMVector3 vecEnd, int
 	m_vertex1[3].tu = 0.02f;
 	m_vertex1[3].tv = 0.02f;
 
-	m_vertex2[0].position = TMVector3{ vecStart.x - m_fScaleV, vecStart.y, vecStart.z };
-	m_vertex2[1].position = TMVector3{ vecEnd.x - m_fScaleV, vecEnd.y, vecEnd.z };
-	m_vertex2[2].position = TMVector3{ vecEnd.x + m_fScaleV, vecEnd.y, vecEnd.z };
-	m_vertex2[3].position = TMVector3{ vecStart.x + m_fScaleV, vecStart.y, vecStart.z };
+	m_vertex2[0].position = TMVector3{ vecStart.x - m_fScaleH, vecStart.y, vecStart.z };
+	m_vertex2[1].position = TMVector3{ vecEnd.x - m_fScaleH, vecEnd.y, vecEnd.z };
+	m_vertex2[2].position = TMVector3{ vecEnd.x + m_fScaleH, vecEnd.y, vecEnd.z };
+	m_vertex2[3].position = TMVector3{ vecStart.x + m_fScaleH, vecStart.y, vecStart.z };
 
 	m_vertex2[0].tu = 0.02f;
 	m_vertex2[0].tv = 0.98000002f;
@@ -173,10 +173,10 @@ void TMEffectBillBoard3::SetPosition(TMVector3 vecStart, TMVector3 vecEnd)
 	m_vertex1[1].position = TMVector3{ vecEnd.x, vecEnd.y - m_fScaleV, vecEnd.z };
 	m_vertex1[2].position = TMVector3{ vecEnd.x, vecEnd.y + m_fScaleV, vecEnd.z };
 	m_vertex1[3].position = TMVector3{ vecStart.x, vecStart.y + m_fScaleV, vecStart.z };
-	m_vertex2[0].position = TMVector3{ vecStart.x - m_fScaleV, vecStart.y, vecStart.z };
-	m_vertex2[1].position = TMVector3{ vecEnd.x - m_fScaleV, vecEnd.y, vecEnd.z };
-	m_vertex2[2].position = TMVector3{ vecEnd.x + m_fScaleV, vecEnd.y, vecEnd.z };
-	m_vertex2[3].position = TMVector3{ vecStart.x + m_fScaleV, vecStart.y, vecStart.z };
+	m_vertex2[0].position = TMVector3{ vecStart.x - m_fScaleH, vecStart.y, vecStart.z };
+	m_vertex2[1].position = TMVector3{ vecEnd.x - m_fScaleH, vecEnd.y, vecEnd.z };
+	m_vertex2[2].position = TMVector3{ vecEnd.x + m_fScaleH, vecEnd.y, vecEnd.z };
+	m_vertex2[3].position = TMVector3{ vecStart.x + m_fScaleH, vecStart.y, vecStart.z };
 }
 
 void TMEffectBillBoard3::SetShort(unsigned int dwTime)
