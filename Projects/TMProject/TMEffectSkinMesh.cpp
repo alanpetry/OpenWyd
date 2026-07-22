052819c1ac9ab710d0e2f0e2be32a0b597287001
alanpetry <alan_petry@hotmail.com>
2026-07-22 09:06:49 -0300

    Restore EffectSkinMesh blend state after render

--- Projects/TMProject/TMEffectSkinMesh.cpp
+++ Projects/TMProject/TMEffectSkinMesh.cpp
@@ -245,6 +245,8 @@ int TMEffectSkinMesh::Render()
 		g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 7u);
 		g_pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, 1u);
 		g_pDevice->SetRenderState(D3DRS_ZWRITEENABLE, 1u);
+		g_pDevice->SetRenderState(D3DRS_DESTBLEND, 6u);
+		g_pDevice->SetRenderState(D3DRS_FOGENABLE, g_pDevice->m_bFog);
 	}
 
 	return 1;
